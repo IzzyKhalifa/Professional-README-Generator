0935637f80789cb84cacba91c2a883b258df242a
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-10-11 13:58:05 +1100

    Added questions

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .createPromptModule([
+        {
+            type: 'input',
+            name: 'purpose',
+            message : 'What the app is for',
+        },
+        {
+            type: 'input',
+            name: 'use',
+            message: 'How to use the app',
+        },
+        {
+            type: 'input',
+            name: 'report',
+            message: 'How to report issues',
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'How to make contributions',
+        },
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
