9e0865906fd8aa66b9915b1c822b11296b5b1f8a
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-10-11 15:07:48 +1100

    Displays questions in terminal

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-inquirer
-    .createPromptModule([
-        {
-            type: 'input',
-            name: 'purpose',
-            message : 'What the app is for',
-        },
-        {
-            type: 'input',
-            name: 'use',
-            message: 'How to use the app',
-        },
-        {
-            type: 'input',
-            name: 'report',
-            message: 'How to report issues',
-        },
-        {
-            type: 'input',
-            name: 'contributions',
-            message: 'How to make contributions',
-        },
-    ])
+const questions=[
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of the project",
+  },
+  {
+    type: "input",
+    name: "purpose",
+    message: "What the app is for",
+  },
+  {
+    type: "input",
+    name: "use",
+    message: "How to use the app",
+  },
+  {
+    type: 'list',
+    name: 'licenses',
+    message: 'Which license are you using',
+    choices: ['MIT License' , 'Apache License 2.0' , 'GNU License v3.0'],
+  },
+  {
+    type: "checkbox",
+    name: "report",
+    message: "How to report issues",
+    choices: ["Email", "Phone", "Slack"],
+  },
+  {
+    type: "checkbox",
+    name: "contributions",
+    message: "How to make contributions",
+    choices: ["Email", "Phone", "Slack"],
+  }
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+function writeToFile() {
+    const fileName = 'README.md';
+    inquirer.prompt(questions)
+    .then((response) => {  
+        console.log("Generating a ReadMe");
+        const markdown = generateMarkdown(response);
+        console.log(markdown);
+        fs.writeFileSync(fileName,response.toString());
+});
     
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+    
+}
 
 // Function call to initialize app
 init();
